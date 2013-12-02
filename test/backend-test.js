9fcd0ed80aea676681d696cc84e2d7589da13c0e
Aleksey Kulikov <AlekseyKulikov@hotmail.com>
2013-12-02 13:02:24 +0600

    Add tests for `put`

--- test/backend-test.js
+++ test/backend-test.js
@@ -49,8 +49,105 @@ function backendSpec(name, backend) {
         ], function(err, result) {
           expect(result[0]).equal(123);
           expect(result[1]).eql([1, 2, 3, 4]);
-          expect(result[2]).equal(null);
+          expect(result[2]).null;
           done(err);
+        });
+      });
+
+      it('allows parallel reading', function(done) {
+        var tags = indexed('notepad:tags');
+        tags.batch([
+          { type: 'put', key: '1', value: { name: 'tag 1' } },
+          { type: 'put', key: '2', value: { name: 'tag 2' } },
+        ], function(err) {
+          async.parallel([
+            function(cb) { notes.get('1', cb) },
+            function(cb) { tags.get('1', cb) },
+            function(cb) { notes.get('baz', cb) },
+          ], function(err2, result) {
+            if (err || err2) return done(err || err2);
+
+            expect(result[0]).eql({ name: 'note 1' });
+            expect(result[1]).eql({ name: 'tag 1' });
+            expect(result[2]).null;
+            tags.clear(done);
+          });
+        });
+      });
+    });
+
+    describe('put', function() {
+      afterEach(function(done) {
+        indexed.dropDb('notepad', done);
+      });
+
+      it('put one value', function(done) {
+        var tags = indexed('notepad:tags');
+
+        tags.put('foo', 'bar', function(err) {
+          tags.get('foo', function(err2, tag) {
+            expect(tag).equal('bar');
+            done(err || err2);
+          });
+        });
+      });
+
+      it('replace existing value', function(done) {
+        var notes = indexed('notepad:notes');
+
+        async.series([
+          function(cb) { notes.get('foo', cb) },
+          function(cb) { notes.put('foo', { value: 'bar', sub: 'baz' }, cb) },
+          function(cb) { notes.get('foo', cb) },
+          function(cb) { notes.put('foo', { value: 'baz' }, cb) },
+          function(cb) { notes.get('foo', cb) },
+        ], function(err, result) {
+          expect(result[0]).undefined;
+          expect(result[2]).eql({ value: 'bar', sub: 'baz' });
+          expect(result[4]).eql({ value: 'baz' });
+          done(err);
+        });
+      });
+
+      it('allows parallel writing', function(done) {
+        var notebooks = indexed('notepad:notebooks');
+        var tags = indexed('notepad:tags');
+
+        async.parallel([
+          function(cb) { notebooks.put('foo', { value: 1 }, cb) },
+          function(cb) { tags.put('foo', { value: 'bar', sub: 'baz' }, cb) },
+          function(cb) { tags.put('2', true, cb) },
+          function(cb) { notebooks.put('3', false, cb) },
+          function(cb) { tags.put('3', 'foobar', cb) },
+        ], function(err) {
+          async.parallel([
+            function(cb) { notebooks.get('foo', cb) },
+            function(cb) { tags.get('foo', cb) },
+            function(cb) { tags.get('2', cb) },
+            function(cb) { notebooks.get('3', cb) },
+            function(cb) { tags.get('3', cb) },
+          ], function(err2, result) {
+            expect(result[0]).eql({ value: 1 });
+            expect(result[1]).eql({ value: 'bar', sub: 'baz' });
+            expect(result[2]).true;
+            expect(result[3]).false;
+            expect(result[4]).equal('foobar');
+            done(err || err2);
+          });
+        });
+      });
+
+      it.skip('adds only one value on each put', function(done) {
+        var notebooks = indexed('notepad:notebooks');
+        async.series([
+          function(cb) { notebooks.put('1', { name: 'notebook 1' }, cb) },
+          function(cb) { notebooks.put('2', { name: 'notebook 2' }, cb) },
+          function(cb) { notebooks.put('1', { name: '1' }, cb) },
+        ], function(err) {
+          notebooks.query({}, function(err2, values) {
+            expect(values).length(2);
+            done(err || err2);
+          });
         });
       });
     });
