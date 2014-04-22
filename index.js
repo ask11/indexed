ee05a3753c57f1ceb5c1a53d178e31b710110a7b
Aleksey Kulikov <AlekseyKulikov@hotmail.com>
2014-04-22 10:21:13 +0600

    refactoring

--- index.js
+++ index.js
@@ -1,42 +1,19 @@
 var DB = require('./lib/db');
 var Store = require('./lib/store');
-var type = require('type');
+var Transaction = require('./lib/transaction');
 
 /**
- * Expose `indexed()`.
+ * Expose `DB`.
  */
 
-module.exports = exports = indexed;
-
-/**
- * Create new `DB` with `name`.
- *
- * @param {String} name
- * @param {Number} [version]
- * @param {Function} [cb]
- * @return {DB}
- */
-
-function indexed(name, version, cb) {
-  if (type(version) == 'function') {
-    cb = version;
-    version = 1;
-  }
-
-  return new DB(name, version, cb);
-}
+module.exports = exports = DB;
 
 /**
  * Aliases.
  */
 
-exports.open = indexed;
-
-/**
- * Expose core classes.
- */
-
-exports.DB = DB; // db interface
-exports.Store = Store; // stores
-exports.Transaction; // transactions
-exports.Index; // subclass of Store to manage indexes
+exports.open = DB;
+exports.DB = DB;
+exports.Store = Store;
+exports.Transaction = Transaction;
+exports.Index;
